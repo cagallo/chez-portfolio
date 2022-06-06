a8e2bd6e37094203dc74a18f76f9e9001b56354c
Chez <cesaregallo0@gmail.com>
2022-06-06 13:33:36 -0600

    Add styling to Contact and Nav components

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,11 @@
+import './Contact.scss'
+
 const Contact = () => {
     return (
-        <section id='Contact'>
-            <p>I am not looking for any new opportunities, but my inbox is always open. Please reach out if you have a question or just want to say hello. I will try my best to get back to you promptly</p>
-            <a href="mailto:cesaregallo0@gmail.com">Say Hello</a>
+        <section className='Contact'>
+            <h1 className='header'>05. Get In Touch</h1>
+            <p className="msg">I am not looking for any new opportunities, but my inbox is always open. Please reach out if you have a question or just want to say hello. I will try my best to get back to you promptly</p>
+            <a className="mailer" href="mailto:cesaregallo0@gmail.com">Say Hello</a>
        </section>
     )
 }
