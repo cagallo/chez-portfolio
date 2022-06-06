2795c27a4eb09795d54df077e81c69b61add8d37
Chez <cesaregallo0@gmail.com>
2022-06-06 17:55:15 -0600

    Add network request to Work and styling to project cards

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react"
+import ProjectCard from "../ProjectCard/ProjectCard"
+import './Work.scss'
+
 const Work = () => {
+    
+    const [projects, setProjects] = useState([])
+    const [error, setError] = useState('')
+ 
+    
+    const fetchProjects = async() => {
+        const url = 'https://chez-portfolio-api.herokuapp.com/projects'
+        setError('')
+
+        try {
+            const response = await fetch(url)
+            const projects = await response.json()
+            console.log(projects)
+            const reversedProjects = projects.reverse()
+            setProjects(reversedProjects)
+        } catch(error) {
+            setError(error.message)
+        }
+    }
+    
+    useEffect(() => {
+        fetchProjects()
+    }, [])
+
     return (
-        <h2>Check out my projects</h2>
+        <div className="projects">
+            <h1 className="projects-headline">04. Check Out My Projects</h1>
+            { error && error }
+            <section className="projects-grid">
+                { projects.map(project => {
+                    console.log(project)
+                    return (<ProjectCard key={ project.id } project={ project } />)
+                }) }
+            </section>
+        </div>
     )
 }
 
