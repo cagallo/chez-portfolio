eacb709a42856a7cd6803ad0ed97b49288521db6
Chez Gallo <cesaregallo0@gmail.com>
2024-02-19 14:48:20 -0700

    Update contact link target

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,24 @@
-import "./Contact.scss";
+import './Contact.scss';
 
 const Contact = () => {
-	return (
-		<section className="Contact">
-			<h1 className="header">04. Get In Touch</h1>
-			<p className="msg">
-				I am looking for any new opportunities and my inbox is always open.
-				Please reach out if you have a question or just want to say hello. I
-				will try my best to get back to you promptly. 
-			</p>
-			<a className="mailer" href="mailto:cesaregallo0@gmail.com">
-				Say Hello
-			</a>
-		</section>
-	);
+  return (
+    <section className='Contact'>
+      <h1 className='header'>04. Get In Touch</h1>
+      <p className='msg'>
+        I am looking for any new opportunities and my inbox is always open.
+        Please reach out if you have a question or just want to say hello. I
+        will try my best to get back to you promptly.
+      </p>
+      <a
+        className='mailer'
+        href='mailto:cesaregallo0@gmail.com'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Say Hello
+      </a>
+    </section>
+  );
 };
 
 export default Contact;
